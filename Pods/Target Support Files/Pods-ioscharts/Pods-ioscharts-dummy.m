0d6d1679d87348356386f8596ae2e8fef0a32a94
Dan Beaulieu <daniel.beaulieu@eightbot.com>
2018-02-11 21:52:18 -0600

    initial commit

---
+++ Pods/Target Support Files/Pods-ioscharts/Pods-ioscharts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ioscharts : NSObject
+@end
+@implementation PodsDummy_Pods_ioscharts
+@end
--- ioscharts/ViewController.swift
+++ ioscharts/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var lineChartView: LineChartView!
+    
+    @IBAction func randomize(_ sender: UIButton) {
+        let count = Int(arc4random_uniform(20) + 3)
+        setChartValues(count)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setChartValues()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setChartValues(_ count : Int = 20) {
+        let values = (0..<count).map { (i) -> ChartDataEntry in
+            let val = Double(arc4random_uniform(UInt32(count)) + 3)
+            return ChartDataEntry(x: Double(i), y: val)
+        }
+        
+        let set1 = LineChartDataSet(values: values, label: "DataSet 1")
+        let data = LineChartData(dataSet: set1)
+        
+        self.lineChartView.data = data
+        
     }
-
 
 }
 
